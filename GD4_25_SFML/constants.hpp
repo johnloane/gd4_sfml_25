c6a82e8e26af5cee6e9bb5f5ba724fbd07477097
johnloane <john.loane@dkit.ie>
2025-09-23 15:32:05 +0100

    Create the baseline game
    Make sure you add SFML 3 in the Project directory and use Build
    Events to copy the dlls to where .exe is.

---
+++ GD4_25_SFML/constants.hpp
@@ -0,0 +1,3 @@
+#pragma once
+constexpr auto kPlayerSpeed = 100.f;
+constexpr auto kTimePerFrame = 1 / 60.f;
---
+++ GD4_25_SFML/game.cpp
@@ -0,0 +1,97 @@
+#include "game.hpp"
+#include "utility.hpp"
+#include "constants.hpp"
+
+Game::Game()
+	: m_window(sf::VideoMode({ 640, 480 }), "SFML Refactor"), m_player()
+{
+	m_player.setRadius(40.f);
+	m_player.setPosition({ 100.f, 100.f });
+	m_player.setFillColor(sf::Color::Cyan);
+}
+
+void Game::Run()
+{
+	sf::Clock clock;
+	sf::Time time_since_last_update = sf::Time::Zero;
+	while (m_window.isOpen())
+	{
+		time_since_last_update += clock.restart();
+		while(time_since_last_update.asSeconds() > kTimePerFrame)
+		{
+			time_since_last_update -= sf::seconds(kTimePerFrame);
+			ProcessEvents();
+			Update(sf::seconds(kTimePerFrame));
+		}	
+		Render();
+	}
+}
+
+void Game::ProcessEvents()
+{
+	while (const std::optional event = m_window.pollEvent())
+	{
+		if (event->is<sf::Event::Closed>())
+		{
+			m_window.close();
+		}
+		else if (const auto* KeyPressed = event->getIf<sf::Event::KeyPressed>())
+		{
+			HandlePlayerInput(KeyPressed->scancode, true);
+		}
+		else if (const auto* KeyReleased = event->getIf<sf::Event::KeyReleased>())
+		{
+			HandlePlayerInput(KeyReleased->scancode, false);
+		}
+	}
+}
+
+void Game::Update(sf::Time delta_time)
+{
+	sf::Vector2f movement(0.f, 0.f);
+	if (m_is_moving_up)
+	{
+		movement.y -= 1;
+	}
+	if (m_is_moving_down)
+	{
+		movement.y += 1;
+	}
+	if (m_is_moving_left)
+	{
+		movement.x -= 1;
+	}
+	if (m_is_moving_right)
+	{
+		movement.x += 1;
+	}
+	m_player.move(Utility::Normalise(movement) * kPlayerSpeed * delta_time.asSeconds());
+
+}
+
+void Game::Render()
+{
+	m_window.clear();
+	m_window.draw(m_player);
+	m_window.display();
+}
+
+void Game::HandlePlayerInput(sf::Keyboard::Scancode key, bool is_pressed)
+{
+	if (key == sf::Keyboard::Scancode::W)
+	{
+		m_is_moving_up = is_pressed;
+	}
+	if (key == sf::Keyboard::Scancode::S)
+	{
+		m_is_moving_down = is_pressed;
+	}
+	if (key == sf::Keyboard::Scancode::A)
+	{
+		m_is_moving_left = is_pressed;
+	}
+	if (key == sf::Keyboard::Scancode::D)
+	{
+		m_is_moving_right = is_pressed;
+	}
+}
